9e1e947797825d4075c3c005cd92cc7a23d5dd8b
Varinia <v.mesias.r@gmail.com>
2025-10-02 04:22:06 +0000

    tamaño carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function formarCartaAleatoria() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  let pinta = ['♦', '♥', '♠', '♣'];
+    let randomNumero = numero[Math.floor(Math.random() * numero.length)];
+    let randomPinta = pinta[Math.floor(Math.random() * pinta.length)];
+    document.getElementsByClassName('card').innerText=`${randomNumero} ${randomPinta}`;
 };
+
+
