f423f59d8aa0bed8973f65344eebda0d4c7d3d5f
Varinia <v.mesias.r@gmail.com>
2025-10-03 05:03:06 +0000

    colores y tamaño

--- src/app.js
+++ src/app.js
@@ -4,14 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function formarCartaAleatoria() {
+window.onload = function() {
   //write your code here
+  //llamar aleatoriamente
   let numero = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  let pinta = ['♦', '♥', '♠', '♣'];
-    let randomNumero = numero[Math.floor(Math.random() * numero.length)];
-    let randomPinta = pinta[Math.floor(Math.random() * pinta.length)];
-    document.getElementsByClassName('card').innerText=`${randomNumero} ${randomPinta}`;
-};
+  let randomNumero = numero[Math.floor(Math.random() * numero.length)];
+  document.getElementById('number').innerText=`${randomNumero}`;
+  const pinta = ['♦', '♥', '♠', '♣'];
+  const iconElements = document.querySelectorAll('.icon');
+  const indiceAleatorio = Math.floor(Math.random() * pinta.length);
+  const pintaAleatoria = pinta[indiceAleatorio];
+  iconElements.forEach(el => {
+  el.textContent = pintaAleatoria;
+//propiedades iconos
+if (pintaAleatoria === '♦' || pintaAleatoria === '♥'){
+  el.style.color= 'red';
+} else {
+  el.style.color= 'black';
+  }
+});
+}
 
 
