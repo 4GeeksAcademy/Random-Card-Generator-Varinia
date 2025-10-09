8de351096e09fbf097a2b168f5d282a7fa2a4efb
Varinia <v.mesias.r@gmail.com>
2025-10-09 15:27:47 +0000

    correccion funcion

--- src/app.js
+++ src/app.js
@@ -7,28 +7,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   //write your code here
   //llamar aleatoriamente
-  let numero = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  let randomNumero = numero[Math.floor(Math.random() * numero.length)];
-  document.getElementById('number').innerText = `${randomNumero}`;
-  const pinta = ['♦', '♥', '♠', '♣'];
-  const iconElements = document.querySelectorAll('.icon');
-  const indiceAleatorio = Math.floor(Math.random() * pinta.length);
-  const pintaAleatoria = pinta[indiceAleatorio];
-  iconElements.forEach(el => {
-    el.textContent = pintaAleatoria;
-    if (pintaAleatoria === '♦' || pintaAleatoria === '♥') {
-      el.style.color = 'red';
-    } else {
-      el.style.color = 'black';
-    }
-  });
-  const parrafo = document.getElementById('number');
-  parrafo.style.textAlign = 'center';
-let botonRecargar = document.getElementById('btn-recargar');
-botonRecargar.addEventListener('click', function () {
-  location.reload();
-})
-setTimeout(generarCarta, 10000);
+  function generarCarta() {
+    const numero = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const pinta = ['♦', '♥', '♠', '♣'];
+    let randomNumero = numero[Math.floor(Math.random() * numero.length)];
+    const indiceAleatorio = Math.floor(Math.random() * pinta.length);
+    document.getElementById('number').innerText = `${randomNumero}`;
+    const parrafo = document.getElementById('number');
+    parrafo.style.textAlign = 'center';
+    const iconElements = document.querySelectorAll('.icon');
+    const pintaAleatoria = pinta[indiceAleatorio];
+    iconElements.forEach(el => {
+      el.textContent = pintaAleatoria;
+      el.style.color = (pintaAleatoria === '♦' || pintaAleatoria === '♥') ? 'red' : 'black';
+    });
+  }
+  generarCarta();
+  const botonRecargar = document.getElementById('btn-recargar');
+  botonRecargar.addEventListener('click', generarCarta);
+  setTimeout(generarCarta, 10000);
 }
 
 
